cb3ff21f499e6a89bd752ab080f50a5b5c80fbb7
Alex Schmidt <alex.schmidt767@gmail.com>
2022-08-03 18:04:39 -0500

    finished coding main game

---
+++ src/sound.py
@@ -0,0 +1,11 @@
+from importlib.resources import path
+import pygame
+
+class Sound:
+
+    def __init__(self, path) -> None:
+        self.path = path
+        self.sound = pygame.mixer.Sound(path)
+
+    def play(self):
+        pygame.mixer.Sound.play(self.sound)
